763c7423bd6e2558662d8d131cb0a43cc069ac16
Guilherme Hammes <gffhammes@gmail.com>
2023-08-15 23:18:14 -0300

    feat: change bg color and add text color to node

--- src/Node/CustomNode/CustomNodeContent.tsx
+++ src/Node/CustomNode/CustomNodeContent.tsx
@@ -1,22 +1,5 @@
 import { Box, Stack, Typography } from "@mui/material";
-import { ISelectOption, SelectInput } from "../../Form/SelectInput";
-import { useState } from "react";
 import { IDimentions } from "../../interfaces";
-
-const options: ISelectOption[] = [
-  {
-    label: "Cachorro",
-    value: 0,
-  },
-  {
-    label: "Gato",
-    value: 1,
-  },
-  {
-    label: "Papagaio",
-    value: 2,
-  },
-];
 
 export interface ICustomNodeContentProps {
   dimentions: IDimentions;
@@ -27,23 +10,18 @@ export const CustomNodeContent = ({
   dimentions,
   name,
 }: ICustomNodeContentProps) => {
-  const [selectedValue, setSelectedValue] = useState(options[0]);
-
-  const handleChange = (newValue: ISelectOption) => {
-    setSelectedValue(newValue);
-  };
-
   return (
-    <Box sx={{ ...dimentions, backgroundColor: "#f4f4f4", px: 4, py: 3 }}>
+    <Box
+      sx={{
+        ...dimentions,
+        backgroundColor: "#f4f4f4",
+        color: "black",
+        px: 4,
+        py: 3,
+      }}
+    >
       <Stack spacing={4}>
         <Typography>{name}</Typography>
-
-        {/* <SelectInput
-          label="Selecione"
-          options={options}
-          value={selectedValue}
-          onChange={handleChange}
-        /> */}
       </Stack>
     </Box>
   );
