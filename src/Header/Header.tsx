3a9d474dba8b77f3a8644baf4cfb6e8a3d137020
Guilherme Hammes <gffhammes@gmail.com>
2023-09-26 21:35:13 -0300

    feat: remove header garbage

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,19 +1,7 @@
-import { Button, Paper, Stack, Typography } from "@mui/material";
-import { CheckboxFormGroup } from "../Form/CheckboxFormGroup";
-import { useFilterContext } from "../FilterContext/useFilterContext";
-import { filterOptions } from "../Flow/context/FlowContextProvider";
-import { useFlowContext } from "../Flow/context/useFlowContext";
+import { Paper, Stack, Typography } from "@mui/material";
 import { HeaderActions } from "./HeaderActions";
 
 export const Header = () => {
-  const { selectedConnectors, handleSelectedConnectorsChange } =
-    useFilterContext();
-
-  const selected = filterOptions.filter(
-    (option) =>
-      !!selectedConnectors.find((connector) => connector.value === option.value)
-  );
-
   return (
     <Paper variant="outlined" square sx={{ width: "100%", px: 4, py: 2 }}>
       <Stack
@@ -26,13 +14,6 @@ export const Header = () => {
 
         <HeaderActions />
       </Stack>
-
-      {/* <CheckboxFormGroup
-        label="Conexão"
-        options={filterOptions}
-        selectedOptions={selected}
-        handleChange={handleSelectedConnectorsChange}
-      /> */}
     </Paper>
   );
 };
