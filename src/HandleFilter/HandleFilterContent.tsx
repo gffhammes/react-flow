95931ae180042133d08f129657939061c9de6088
Guilherme Hammes <gffhammes@gmail.com>
2023-09-26 21:04:16 -0300

    feat: make filter checkbox work

--- src/HandleFilter/HandleFilterContent.tsx
+++ src/HandleFilter/HandleFilterContent.tsx
@@ -1,8 +1,8 @@
 import { Box, Typography } from "@mui/material";
 import { handlesOptions } from "../Handles/handles";
 import { IHandle, THandleClass } from "../interfaces";
-import { CheckboxGroupWithParent } from "../Form/CheckboxGroupWithParent";
 import { useState } from "react";
+import { CheckboxGroupsWithParents } from "../Form/CheckboxGroupsWithParents";
 
 export interface IHandleFilterContentProps {}
 
@@ -32,18 +32,16 @@ export const HandleFilterContent = () => {
     <Box sx={{ p: 2 }}>
       <Typography variant="caption">FILTROS</Typography>
 
-      {groups.map((group) => (
-        <Box key={group.groupLabel}>
-          <CheckboxGroupWithParent
-            options={group.handles}
-            parentLabel={group.groupLabel}
-            getOptionLabel={(option) => option.name}
-            getOptionValue={(option) => option.id}
-            selectedOptions={selectedHadles}
-            onChange={(value) => setSelectedHandles(value)}
-          />
-        </Box>
-      ))}
+      <CheckboxGroupsWithParents<any>
+        groups={groups.map((group) => ({
+          options: group.handles,
+          getOptionLabel: (option) => option.name,
+          getOptionValue: (option) => option.id,
+          label: group.groupLabel,
+        }))}
+        selectedOptions={selectedHadles}
+        handleChange={(newValues) => setSelectedHandles(newValues)}
+      />
     </Box>
   );
 };
